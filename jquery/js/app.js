aa4618cbb2fefcea6870ca896fb2345c1b7f153f
Kevin Yan <kevintheyan@gmail.com>
2015-10-15 17:10:30 -0700

    classwork

--- jquery/js/app.js
+++ jquery/js/app.js
@@ -1,3 +1,5 @@
+console.log('I AM KEVITAR');
+
 /*
     script for the index.html page
     dependencies: jquery
@@ -5,4 +7,23 @@
     open weather API: 
     http://api.openweathermap.org/data/2.5/weather?zip=98195,us&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f
 */
+$(function () {
+    'use strict';
+   $('a').attr('target', '_blank');
+    $('article').hide().fadeIn(1000);
+    $('#toggle-article').click(function() {
+        $('article').fadeToggle();
+    });
+
+    $.getJSON('http://api.openweathermap.org/data/2.5/weather?zip=98195,us&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f')
+        .then(function(data) {
+            console.log(data);
+            var temperature = data.main.temp;
+            $('#temp').text(temperature);
+        });
+
+    console.log('test');
+});
+
+
 
--- leaflet/js/app.js
+++ leaflet/js/app.js
@@ -8,3 +8,29 @@
     attribution:
     '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 */
+
+$(function() {
+    'use strict';
+    function createMap(loc, zoom) {
+
+        var map = L.map('map').setView(loc, zoom);
+
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.circleMarker(loc).addTo(map).bindPopup('Hello');
+    }
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(pos) {
+            createMap([pos.coords.latitude, pos.coords.longitude], 15);
+        });
+    }
+    else {
+        createMap([47.6067, -122.3331], 12);
+    }
+});
+
+// acquire json, iterate over the array, each object has lat/long of camera, add a marker for each camera
+// add style to popups
